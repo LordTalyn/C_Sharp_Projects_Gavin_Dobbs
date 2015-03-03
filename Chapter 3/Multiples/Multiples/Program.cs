6ba49fbb4c317eb959491b7ebf4f0178f6f51f52
Gavin Dobbs <gdobbs0719@outlook.com>
2015-03-02 20:21:56 -0800

    Beginning Work in Chapter 3

---
+++ Chapter 3/Multiples/Multiples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write an app that reads two integers, determines whether the first is a multiple
+            of the second and displays the result. [Hint: Use the remainder operator.]*/
+            int number1;
+            int number2;
+
+            Console.Write("Please enter the first number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Now enter the second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            if (number1 % number2 == 0)
+                Console.WriteLine("Your first number is a multiple of the second");
+            if (number1 % number2 != 0)
+                Console.WriteLine("This is not a multiple situation");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter 3/Odd or Even/Odd or Even/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_or_Even
+{
+    class Program
+    {
+        /*Write an app that reads an integer, then determines and displays whether
+        it’s odd or even. [Hint: Use the remainder operator. An even number is a multiple of 2. Anymultiple
+        of 2 leaves a remainder of 0 when divided by 2.]*/
+        static void Main(string[] args)
+        {
+            int digit;
+            Console.Write("Please enter an integer: ");
+            digit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            if (digit % 2 == 0)
+            Console.Write("Your number is even");
+            if (digit % 2 != 0)
+                Console.WriteLine("Your number is odd");
+            Console.ReadLine();
+        }
+    }
+}
